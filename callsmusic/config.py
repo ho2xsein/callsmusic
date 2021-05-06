0710b3e1799d7d4ceb27f3fc98bfd2e2f4f05671
ho2xsein <59254677+ho2xsein@users.noreply.github.com>
2021-05-06 17:40:39 +0430

    Update config.py

--- callsmusic/config.py
+++ callsmusic/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 SESSION_NAME = getenv('SESSION_NAME', 'session')
-BOT_TOKEN = getenv('BOT_TOKEN')
-API_ID = int(getenv('API_ID', ''))
-API_HASH = getenv('API_HASH')
-DURATION_LIMIT = int(getenv('DURATION_LIMIT', '7'))
+BOT_TOKEN = '386893380:AAGQzP4kmQSayOkYs6jp7AV_q3dI5ihNpaQ'
+API_ID = 5378568
+API_HASH = '89ffcb590951a70b2f00d51a3689458b'
+DURATION_LIMIT = 10
 COMMAND_PREFIXES = list(getenv('COMMAND_PREFIXES', '/ !').split())
 SUDO_USERS = list(map(int, getenv('SUDO_USERS', '').split()))
